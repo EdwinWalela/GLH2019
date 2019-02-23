2e238b62f878af706a0841e1d74e5d66f5109cf6
Miller Adulu <milleradulu@gmail.com>
2019-02-23 15:30:28 +0300

    Allow a user to view their land record

---
+++ src/clients/components/UserLandRecord.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-layout>
+    <v-flex xs12 >
+      <v-card v-if="record">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Land Record</h3>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-title>
+                Title Number
+              </v-list-tile-title>
+              <v-list-tile-sub-title>{{ record.titleNo }}</v-list-tile-sub-title>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-title>
+                Owner Address
+              </v-list-tile-title>
+              <v-list-tile-sub-title>{{ record.ownAddress }}</v-list-tile-sub-title>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-title>
+                Sheet Number
+              </v-list-tile-title>
+              <v-list-tile-sub-title>{{ record.sheetNo }}</v-list-tile-sub-title>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-title>
+                Owners
+              </v-list-tile-title>
+              <v-list-tile-sub-title>{{ record.owners }}</v-list-tile-sub-title>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-title>
+                Lawyers
+              </v-list-tile-title>
+              <v-list-tile-sub-title>{{ record.lawyers }}</v-list-tile-sub-title>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-title>
+                Date
+              </v-list-tile-title>
+              <v-list-tile-sub-title>{{ moment(record.date).format("dddd, MMMM Do YYYY") }}</v-list-tile-sub-title>
+            </v-list-tile>
+
+          </v-list>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block color="green">List for Sale</v-btn>
+        </v-card-actions>
+
+      </v-card>
+    </v-flex>
+  </v-layout>
+
+</template>
+
+<script>
+import axios from 'axios';
+import moment from 'moment';
+
+export default {
+  name: 'UserLandRecord',
+  data() {
+    return {
+      loading: true,
+      record: null,
+      moment
+    };
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get('https://5c711f4a0eddba001435b5d1.mockapi.io/land/1')
+        .then((response) => {
+          this.record = response.data;
+          this.loading = false;
+        })
+        .catch(error => console.log(error));
+    },
+  },
+  computed: {
+    date() {
+      this.record.date
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
+</script>
