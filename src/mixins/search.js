03c10d6092eccf33fe4527f1e4dc19c399c4937d
Miller Adulu <milleradulu@gmail.com>
2019-02-23 19:06:27 +0300

    Remove logs

--- src/mixins/search.js
+++ src/mixins/search.js
@@ -10,17 +10,13 @@ export default {
   },
   methods: {
     async fetchData() {
-      console.log('Title:', this.titleNumber);
       const deedAddress = await bank.methods.lookUp(this.titleNumber).call();
       const deed = await new web3.eth.Contract(
         JSON.parse(compileDeed.interface),
         deedAddress,
       );
-      console.log('Deed: ', deed);
-      console.log('Deed Addr', deedAddress);
 
       const transOwners = await deed.methods.owners().call();
-      console.log(transOwners);
       const forSale = await deed.methods.forSale().call();
       this.record = {
         owners: transOwners.owners,
