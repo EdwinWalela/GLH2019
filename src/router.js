94f0ec8d7d5b5f61736a9c60393fb9fa98aa9065
Miller Adulu <milleradulu@gmail.com>
2019-02-23 14:26:22 +0300

    Add routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,47 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+/**
+ * Registrar
+ */
+
+import Registrar from '@/registrar/Registrar.vue';
+import ViewLandRecords from '@/registrar/components/ViewLandRecords.vue';
+/**
+ * Client
+ */
+
+import Client from '@/clients/Client.vue';
+
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [],
+  routes: [
+    {
+      path: '/client',
+      component: Client,
+      children: [
+        // {
+        //   path: '/',
+        //   component: ,
+        // }
+      ],
+    },
+    {
+      path: '/registrar',
+      component: Registrar,
+      children: [
+        {
+          path: '/',
+        },
+        {
+          path: 'records',
+          name: 'viewLandRecords',
+          component: ViewLandRecords,
+        },
+      ],
+    },
+  ],
 });
