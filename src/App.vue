63cc4ce6146495d613da05da7fdef1139400a735
Miller Adulu <milleradulu@gmail.com>
2019-02-23 12:13:36 +0300

    Add metamask implementation
    Detect the metamask credentials and details once the dapp loads

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,34 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <vue-metamask @onComplete="onComplete"/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import VueMetamask from 'vue-metamask';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    VueMetamask,
   },
   data() {
-    return {};
+    return {
+      message: 'This is a demo net at work',
+    };
+  },
+  methods: {
+    onComplete(data) {
+      console.log('MetaMask Data: ', data);
+    },
   },
 };
 </script>
